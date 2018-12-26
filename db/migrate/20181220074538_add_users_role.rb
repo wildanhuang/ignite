d874400a7f3d8d7ed62220d7f074ff3bfc6497b7
Wildan <wildan.p@kiranatama.com>
2018-12-26 17:14:08 +0700

    Fixed default users.role

--- db/migrate/20181220074538_add_users_role.rb
+++ db/migrate/20181220074538_add_users_role.rb
@@ -1,5 +1,5 @@
 class AddUsersRole < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :role, :string, defalut: 'user'
+    add_column :users, :role, :string, null: false, default: 'user'
   end
 end
