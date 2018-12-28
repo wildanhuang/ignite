68dd3dd10de813c74ef059071757509b55d9f1f6
Wildan <wildan.p@kiranatama.com>
2018-12-28 18:07:52 +0700

    update host

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  default_url_options :host => "google.com"
+  default_url_options :host => "http://128.199.144.160"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "dashboards#index"
