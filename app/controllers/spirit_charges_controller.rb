db0ff8c7d9140cce1ded1f66cb50229eaa59f23c
Wildan <wildan.p@kiranatama.com>
2018-12-29 12:39:54 +0700

    Add spirit charges new - form

--- app/controllers/spirit_charges_controller.rb
+++ app/controllers/spirit_charges_controller.rb
@@ -3,4 +3,8 @@ class SpiritChargesController < ApplicationController
   def index
     @spirit_charges = SpiritCharge.all.order(created_at: :asc)
   end
+
+  def new
+    @spirit_charge = SpiritCharge.new
+  end
 end
