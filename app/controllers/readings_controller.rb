b7657163ed2da5f0fc9aeb55b9be68c042b4065a
Wildan <wildan.p@kiranatama.com>
2019-01-01 12:23:26 +0700

    Add user and progress bar

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -1,5 +1,6 @@
 class ReadingsController < ApplicationController
   def index
     @readings = current_user.readings
+    @spirit_charge_size = SpiritCharge.count.to_f
   end
 end
--- app/helpers/readings_helper.rb
+++ app/helpers/readings_helper.rb
@@ -1,2 +1,5 @@
 module ReadingsHelper
+  def readings_progress
+    (@readings.size.to_f / @spirit_charge_size * 100).ceil.to_s
+  end
 end
