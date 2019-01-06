d02b797ffbe552ec3ac492fd3a8cece338205f7c
Wildan <wildan.p@kiranatama.com>
2019-01-06 12:57:45 +0700

    Update readings to users list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def readings_progress(readings)
+    (readings.size.to_f / SpiritCharge.count.to_f * 100).ceil.to_s
+  end
 end
--- app/helpers/readings_helper.rb
+++ app/helpers/readings_helper.rb
@@ -1,5 +1,2 @@
 module ReadingsHelper
-  def readings_progress
-    (@readings.size.to_f / @spirit_charge_size * 100).ceil.to_s
-  end
 end
